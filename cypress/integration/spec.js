803f7f8da7c7239bd0e6e24b91bde58300291e0c
Phelipe Teles <phelipe_teles@hotmail.com>
2021-03-14 20:24:40 -0300

    Reproduce test failing because of unescaped single quote

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,12 @@
-/// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.intercept(
+      "http://ipeadata2-homologa.ipea.gov.br/api/v1/Metadados('BM12_TJOVER12')",
+      {}
+    ).as("getMetadata");
+
+    cy.visit('http://ipeadata-explorer.surge.sh/serie/BM12_TJOVER12')
+
+    cy.wait("@getMetadata")
   })
 })
